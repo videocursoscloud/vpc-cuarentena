bb3476577f6c533298d65bccde38965403c3579b
Miguel Arranz <videocursoscloud[at]gmail.com>
2020-03-29 16:22:20 -0700

    changes

--- packer/amis/prepare_amazonlinux.sh
+++ packer/amis/prepare_amazonlinux.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-set +x
-mkdir /tmp/packer
-cd /tmp/packer
-rm packer*
 
 yum install wget -y
 wget https://releases.hashicorp.com/packer/1.5.5/packer_1.5.5_linux_amd64.zip
 unzip packer_1.5.5_linux_amd64.zip
-chmod +x ./tmp/packer
+pwd
