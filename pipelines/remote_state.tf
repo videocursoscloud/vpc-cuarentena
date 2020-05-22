6e58fedd4fac2dad9784b8153c7827dadf923dee
Miguel Arranz <videocursoscloud[at]gmail.com>
2020-05-22 05:54:10 -0700

    remote state

--- pipelines/remote_state.tf
+++ pipelines/remote_state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "vpc-cuarentena"
-    key    = "vpc/terraform/pipelines.tfstate"
+    key    = "pipelines/terraform/codebuild.tfstate"
     region = "eu-west-1"
   }
 }
