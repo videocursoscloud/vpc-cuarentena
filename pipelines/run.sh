78c309f35529ac9bd7fbd88a64a4ac52ba55366c
Your Name <you@example.com>
2020-05-30 00:05:31 +0100

    changes

--- pipelines/run.sh
+++
@@ -1,3 +0,0 @@
-aws codebuild start-build --project-name app-codebuild|jq .build.id -r
-aws codebuild start-build --project-name bastion-codebuild|jq .build.id -r
-
---
+++ prepare_agent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yum install unzip wget
+
+wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
+unzip terraform_0.12.26_linux_amd64.zip
+mv terraform /usr/local/bin
