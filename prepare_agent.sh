54fe78a316337792dc46737b7a0ab9e414666fd4
Your Name <you@example.com>
2020-05-30 00:57:12 +0100

    changes on prepare agent.sh

--- prepare_agent.sh
+++ prepare_agent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum install unzip wget
-
+apt update
+apt install unzip wget -y
 wget -q https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 unzip terraform_0.12.26_linux_amd64.zip
 mv terraform /usr/local/bin
